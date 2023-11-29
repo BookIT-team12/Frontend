8112b9a4e5c7822d980e063729775556bf88454a
natasa-dz <dzudzar.n@gmail.com>
2023-11-29 17:09:29 +0100

    Created User.ts model class.

--- src/app/user-account-management/user-account-management.component.ts
+++ src/app/user-account-management/user-account-management.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {User} from "../model/User";
 
 @Component({
   selector: 'app-user-account-management',
   templateUrl: './user-account-management.component.html',
   styleUrls: ['./user-account-management.component.css']
 })
+
 export class UserAccountManagementComponent {
+
+/*
+  userForm:FormGroup;
+    currentUser:User;
+*/
+
+
   name: string = '';
   email: string = '';
   surname:string ='';
@@ -15,8 +25,27 @@ export class UserAccountManagementComponent {
   profilePicture:File |null=null;
   phone:string = '';
 
+
+/*  constructor(private fb: FormBuilder) {
+    this.userForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      name: ['', Validators.required],
+      surname: ['', Validators.required],
+      oldPassword: ['', Validators.required],
+      newPassword: ['', Validators.required],
+      phone: ['', Validators.required],
+      address: ['', Validators.required],
+      // Add other form controls and validations as needed
+    });
+  }*/
+
+  //TODO:na apply cahnges kreiraj user-a
   applyChanges(){
+/*    if(this.userForm.valid){
     console.log('Changes applied!')
+    }else{
+      console.log('Changes failed!')
+    }*/
   }
   deleteAccount(){
     console.log('Account deleted!')
