4083173385ceb188e623e0699977a5a294476f2c
DusanDamjanov <105151463+DusanDamjanov@users.noreply.github.com>
2023-11-30 00:52:54 +0100

    Created Register component and routed it

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from "@angular/core";
 import {provideRouter, RouterModule, Routes} from "@angular/router";
 import {LoginComponent} from "./access-control-module/login/login.component";
+import {RegisterComponent} from "./access-control-module/register/register.component";
 
 const routes: Routes=[
-  {path: "login", component: LoginComponent}
+  {path: "login", component: LoginComponent},
+  {path: "register", component: RegisterComponent}
 ]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
