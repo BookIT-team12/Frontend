9e07cb7317d9de4255be5e0071233677a798ae6d
natasa-dz <dzudzar.n@gmail.com>
2023-11-29 13:48:35 +0100

    Created BookIT navbar based on the FIGMA design.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
-import { NgModule } from '@angular/core';
+import {NgModule} from "@angular/core";
 import { BrowserModule } from '@angular/platform-browser';
-
+import {FormsModule} from "@angular/forms";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
 import { AppComponent } from './app.component';
-
+import { UserAccountManagementComponent } from './user-account-management/user-account-management.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserAccountManagementComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    MatToolbarModule,
+    MatMenuModule,
+    BrowserModule,
+    FormsModule,
+    MatButtonModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+  menuItemClicked(option: string) {
+    console.log(`Selected option: ${option}`);
+    // Add your logic here based on the selected option
+  }
+
+}
