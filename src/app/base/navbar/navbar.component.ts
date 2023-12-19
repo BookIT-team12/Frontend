9b80aa7a3beaf95226a455724fc5d26fdcaa1dab
natasa-dz <dzudzar.n@gmail.com>
2023-12-19 22:44:15 +0100

    Solved multiple marker tag location on the leaflet map and merge conflicts

--- src/app/base/navbar/navbar.component.ts
+++ src/app/base/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
 import {AuthService} from "../../access-control-module/auth.service";
 import {Role} from "../../model/user.model";
