3b0fb82007d167da95f5ab7161992499201417da
DusanDamjanov <105151463+DusanDamjanov@users.noreply.github.com>
2024-01-08 02:12:22 +0100

    Refactoring review service code

--- src/app/service/review.service.ts
+++ src/app/service/review.service.ts
@@ -18,7 +18,6 @@ export class ReviewService{
     const url=`${this.apiUrl}/${id}`;
     return this.http.put<Review>(url, review);
   }
-
   deleteOwnerReview(id: number | null): Observable<string> {
     const url = `${this.apiUrl}/owner/${id}`;
     return this.http.delete<string>(url);
@@ -27,26 +26,14 @@ export class ReviewService{
     const url = `${this.apiUrl}/accommodation/${id}`;
     return this.http.delete<string>(url);
   }
-  getAllReviews(): Observable<Review[]> {
-    const url = `${this.apiUrl}/reviews`;
-    return this.http.get<Review[]>(url);
-  }
-
-  getReviewById(id: number): Observable<Review> {
-    const url = `${this.apiUrl}/reviews/${id}`;
-    return this.http.get<Review>(url);
-  }
-
   getOwnerAverageGrade(email: string): Observable<number>{
     const url = `${this.apiUrl}/averageGrade/owner/${email}`;
     return this.http.get<number>(url)
   }
-
   getAccommodationAverageGrade(id: number): Observable<number>{
     const url = `${this.apiUrl}/averageGrade/accommodation/${id}`;
     return this.http.get<number>(url)
   }
-
   getAllAuthorReviewsOwner(authorEmail:string): Observable<Review[]>{
     const url = `${this.apiUrl}/authorReviews/owners/${authorEmail}`;
     return this.http.get<Review[]>(url);
@@ -55,17 +42,14 @@ export class ReviewService{
     const url = `${this.apiUrl}/authorReviews/accommodations/${authorEmail}`;
     return this.http.get<Review[]>(url);
   }
-
   getAllApprovedReviewsOnOwner(ownerEmail: string): Observable<Review[]>{
     const url = `${this.apiUrl}/ownerReviews/${ownerEmail}`;
     return this.http.get<Review[]>(url);
   }
-
   getAllApprovedReviewsOnOwnerAccommodations(ownerEmail: string): Observable<Review[]>{
     const url = `${this.apiUrl}/ownersAccommodationReviews/${ownerEmail}`;
     return this.http.get<Review[]>(url);
   }
-
   getAllReviewAccommodationForApproval(): Observable<Review[]>{
     const url = `${this.apiUrl}/accommodation/toApprove`;
     return this.http.get<Review[]>(url);
@@ -74,5 +58,5 @@ export class ReviewService{
     const url = `${this.apiUrl}/owner/toApprove`;
     return this.http.get<Review[]>(url);
   }
-
 }
+
